3ae3e3aef83a012879646ca4b360a71cafd8d189
wellington <wellingtonslpbi@hotmail.com>
2020-11-08 17:51:08 -0300

    handle register user app

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -14,6 +14,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationsErrors';
 
 import logoImg from '../../assets/logo.png';
@@ -51,9 +53,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode realizar o logon.',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -121,7 +128,7 @@ const SignUp: React.FC = () => {
             </Form>
 
             <Button onPress={() => formRef.current?.submitForm()}>
-              Entrar
+              Cadastrar
             </Button>
           </Container>
         </ScrollView>
